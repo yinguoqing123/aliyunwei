bf5aded6a7118f05ea05e19c9af1105eb83a0e35
yinguoqing <yinguoqing@shein.com>
2022-03-09 16:46:45 +0800

    [update] 增加人工特征，效果不佳，继续改进

--- aliyunwei/src/main3.py
+++ aliyunwei/src/main3.py
@@ -1,6 +1,10 @@
 
 import imp
 from unicodedata import bidirectional
+
+from scipy import rand
+
+from sklearn.utils import shuffle
 from numpy import pad
 import torch
 from torch import nn, optim
@@ -11,7 +15,7 @@ from torchtext.vocab import vocab
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
 import json
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
 from ema import EMA
 from torch.optim.lr_scheduler import *
 from utils import macro_f1, MyDataSet3
@@ -24,25 +28,34 @@ submit_df = pd.read_csv('../data/submit_df3.csv')
 # train_data = MyDataSet3(train_set[cols])
 # test_data = MyDataSet3(test_set[cols])
 lookup1 = torch.load('../data/word2idx3.pk')
+featmatrix1 = np.load('../data/featmatrix1.npy')
+featmatrix2 = np.load('../data/featmatrix2.npy')
 
 
 class Model(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, featmatrix1=None, featmatrix2=None) -> None:
         super(Model, self).__init__()
         self.emb1 = nn.Embedding(len(lookup1), 16, padding_idx=0)
         self.emb2 = nn.Embedding(88, 16, padding_idx=0)
-        self.lstm = nn.LSTM(48, 32, batch_first=True, bidirectional=False)
+        self.lstm = nn.LSTM(72, 32, batch_first=True, bidirectional=False)
         self.classify = nn.Linear(48, 4)
         self.orthogonal_weights()
         #self.classify.bias.data = torch.tensor([-2.38883658, -1.57741002, -0.57731536, -1.96360971])
+        if not featmatrix1 and not featmatrix2:
+            self.manual_embeddings(torch.tensor(featmatrix1).float(), torch.tensor(featmatrix2).float())
         
     def forward(self, feat):
         feat, server_model, len_seq = feat  # len1 batch_size * sentence_num
         word_emb = self.emb1(feat)  # batch_size * sentence_num * word_num * emb_dim
+        manual_emb1 = self.emb3(feat)
+        manual_emb2 = self.emb4(feat)
         b, s, w, d = word_emb.shape
         server_model = self.emb2(server_model)
         #server_model_clone = torch.clone(server_model).reshape(b, 1, -1).tile(s, 1)
         word_emb = word_emb.reshape(b, s, w*d)
+        manual_emb1 = manual_emb1.reshape(b, s, w*4)
+        manual_emb2 = manual_emb2.reshape(b, s, w*4)
+        word_emb = torch.concat([word_emb, manual_emb1, manual_emb2], dim=-1)
         #word_emb = torch.cat([word_emb, server_model_clone], dim=-1)
         word_emb_pack = pack_padded_sequence(word_emb, len_seq, batch_first=True,enforce_sorted=False)
         word_emb, _ = self.lstm(word_emb_pack) 
@@ -52,9 +65,26 @@ class Model(nn.Module):
         score = self.classify(torch.concat([word_emb, server_model], dim=-1))
         score = F.softmax(score, dim=-1)  # batch_size * 4
         return score
+    
+    def orthogonal_weights(self):
+        for name, param in self.lstm.named_parameters():
+            if 'weight' in name:
+                torch.nn.init.orthogonal_(param)
+            if 'bias' in name:
+                torch.nn.init.zeros_(param)
+                
+    def manual_embeddings(self, featmatrix1, featmatrix2):
+        self.emb3 = nn.Embedding(len(lookup1), 4, padding_idx=0)
+        self.emb4 = nn.Embedding(len(lookup1), 4, padding_idx=0)
+        self.emb3.weight.requires_grad = False
+        self.emb3.weight.data = featmatrix1
+        self.emb4.weight.requires_grad = False
+        self.emb4.weight.data = featmatrix2
 
-model = Model()
+model = Model(featmatrix1, featmatrix2)
 def lr_lambda(epoch):
+    if epoch > 35:
+        return 0.02
     if epoch > 20:
         return 0.1
     else:
@@ -66,7 +96,8 @@ lr_sche = LambdaLR(optimizer, lr_lambda)
 def train_and_evaluate(train_set_, test_set_, submit_set_, name):
     model = Model() 
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
-    lr_sche = LambdaLR(optimizer, lr_lambda)
+    scheduler = ReduceLROnPlateau(optimizer, 'max', factor=0.5, patience=5)
+    #lr_sche = LambdaLR(optimizer, lr_lambda)
     train_set_ = MyDataSet3(train_set_)
     test_df = test_set_
     test_set_ = MyDataSet3(test_set_, mode='test')
@@ -74,7 +105,7 @@ def train_and_evaluate(train_set_, test_set_, submit_set_, name):
     train_data = iter(train_set_)
     test_data = iter(test_set_)
     best_f1 = 0
-    for epoch in range(30):
+    for epoch in range(45):
         running_loss = 0
         for step in range(train_set_.step_max):
             feat, label = next(train_data)
@@ -88,7 +119,7 @@ def train_and_evaluate(train_set_, test_set_, submit_set_, name):
             if step % 50 == 49:
                 print(f"Epoch {epoch+1}, step {step+1}: {running_loss}")
                 running_loss = 0 
-        lr_sche.step()   
+        #lr_sche.step()   
         # epoch 结束后 evaluate
         preds = []
         with torch.no_grad():
@@ -103,6 +134,7 @@ def train_and_evaluate(train_set_, test_set_, submit_set_, name):
             torch.save(model.state_dict(), f'../model3/model_{name}.pt')
         test_df.to_csv(f"../valid3/pred_{name}.csv", index=False)
         print(f"macro F1: {macro_F1}")
+        scheduler.step(macro_F1)
     submit_set = MyDataSet3(submit_set_, mode='predict')
     submit_set_iter = iter(submit_set)
     preds = []
@@ -117,9 +149,16 @@ def train_and_evaluate(train_set_, test_set_, submit_set_, name):
     submit_set_[f'label_{name}'] = preds
     submit_set_.to_csv(f'../submit3/submit_{name}.csv', index=False)
     
-
-for i, (train_idx, test_idx) in enumerate(KFold().split(train_set[['sn', 'fault_time','feature', 'servertype', 'label']])):
+for i, (train_idx, test_idx) in enumerate(KFold(shuffle=True, random_state=2022).split(train_set[['sn', 'fault_time','feature', 'servertype', 'label']])):
     train_set_ = train_set.iloc[train_idx]
     train_set_ = pd.concat([train_set_, train_set_[train_set_.label==0]]).reset_index(drop=True)
     test_set_ = train_set.iloc[test_idx]     
     train_and_evaluate(train_set_, test_set_, submit_df, i)
+    print('=====================================')
+
+# for i, (train_idx, test_idx) in enumerate(StratifiedKFold(shuffle=True, random_state=2022).split(train_set[['sn', 'fault_time','feature', 'servertype', 'label']], train_set['label'])):
+#     train_set_ = train_set.iloc[train_idx]
+#     train_set_ = pd.concat([train_set_, train_set_[train_set_.label==0]]).reset_index(drop=True)
+#     test_set_ = train_set.iloc[test_idx]     
+#     train_and_evaluate(train_set_, test_set_, submit_df, i)
+
